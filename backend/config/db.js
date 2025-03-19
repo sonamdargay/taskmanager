c4b4cc9a3277e6a8482919599d42576ff3a68c96
SvnvmD <someodargay167@gmail.com>
2025-03-19 12:34:29 +1000

    edit db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,11 @@
 const mongoose = require("mongoose");
 
 // Set strictQuery explicitly to suppress the warning
-//mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);  // Remove deprecated options
+    await mongoose.connect(process.env.MONGO_URI); // Remove deprecated options
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error.message);
